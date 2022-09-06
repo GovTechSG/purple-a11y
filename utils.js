f90385fff36ddb8a6e4c5e00ea73dc6aeda934cc
younglim <younglim@users.noreply.github.com>
2022-09-06 14:05:14 +0800

    Check prior zip file exist and remove

--- utils.js
+++ utils.js
@@ -109,6 +109,11 @@ exports.zipResults = async (zipName, resultsPath) => {
   // eslint-disable-next-line global-require
   const { execFile } = require('child_process');
 
+  // Check prior zip file exist and remove
+  if (fs.existsSync(zipName)) {
+    fs.unlink(zipName);  
+  }
+
   // To zip up files recursively )-r) in the results folder path
   // Will only zip up the content of the results folder path with (-j) i.e. junk the path
   const command = '/usr/bin/zip';
