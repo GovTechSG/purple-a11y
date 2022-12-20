84e3842c5e668b671d7108937d96a74916213b3a
Mike Gifford <mike.gifford@civicactions.com>
2022-12-20 15:17:06 -0500

    Adding some inline documentation
    Based on text from ChatGPT

--- crawlers/commonCrawlerFunc.js
+++ crawlers/commonCrawlerFunc.js
@@ -28,6 +28,9 @@ const filterAxeResults = (results, host) => {
   };
 };
 
+/* This function takes a page object from Puppeteer and a host string as arguments 
+and injects the axe-core library into the page. It then runs the library on the page 
+and returns the results of the accessibility tests. */
 exports.runAxeScript = async (page, host) => {
   await Apify.utils.puppeteer.injectFile(page, axeScript);
   const results = await page.evaluate(() => {
@@ -44,18 +47,25 @@ exports.runAxeScript = async (page, host) => {
   return filterAxeResults(results, host);
 };
 
+/* Using a random token as an argument and creates a dataset and request 
+queue using the Apify library, using the provided token as the name for both. 
+It returns an object with the dataset and request queue. */
 exports.createApifySubFolders = async randomToken => {
   const dataset = await Apify.openDataset(randomToken);
   const requestQueue = await Apify.openRequestQueue(randomToken);
   return { dataset, requestQueue };
 };
 
+/* Array containing a single function that takes a crawling context and goto 
+options as arguments. It modifies the goto options to include a 'waitUntil' value of 
+'networkidle2' and a timeout value of 30000. */
 exports.preNavigationHooks = [
   async (_crawlingContext, gotoOptions) => {
     gotoOptions = { waitUntil: 'networkidle2', timeout: 30000 };
   },
 ];
 
+/* Logs error messages */
 exports.handleFailedRequestFunction = async ({ request }) => {
   Apify.utils.log.error(`Failed Request - ${request.url}: ${request.errorMessages}`);
 };
