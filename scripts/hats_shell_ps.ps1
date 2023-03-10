aed86981da099f7fc96b38d98df3d59de576a6f0
younglim <younglim@users.noreply.github.com>
2023-03-10 17:49:15 +0800

    Add ImageMagick and enable hats_shell to be located in purple-hats root

--- scripts/hats_shell_ps.ps1
+++ scripts/hats_shell_ps.ps1
@@ -19,12 +19,24 @@ $env:Path = "$path_to_hats\npm-global;$path_to_hats\npm-global\bin;$env:Path";
 echo "INFO: Set path to git for this session"
 $env:Path = "$env:Path;$path_to_hats\git\bin";
 
-echo "INFO: Set path to purple-hats for this session"
-$env:Path = "$path_to_hats\purple-hats;$env:Path"
- 
 echo "INFO: Set path to Playwright cache for this session"
 $env:PLAYWRIGHT_BROWSERS_PATH = "$path_to_hats\ms-playwright"
- 
+
+echo "INFO: Set path to ImageMagick for this session"
+$env:Path = "$path_to_hats\ImageMagick\bin"
+
+if (Test-Path purple-hats) {
+   echo "INFO: Set path to purple-hats for this session"
+	$env:Path = "$path_to_hats\purple-hats;$env:Path"	
+} else {
+    if (Test-Path package.json) {
+    	echo "INFO: Set path to purple-hats for this session"
+	$env:Path = "$path_to_hats;$env:Path"	
+    } else {
+        Write-Output "Could not find purple-hats"
+    }
+}
+
 echo ""
 $allArgs = $PsBoundParameters.Values + $args + ""
 
