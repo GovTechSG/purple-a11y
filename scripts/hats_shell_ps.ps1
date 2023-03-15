c538b8942c22426687eb10653a4390c4acc04e92
younglim <younglim@users.noreply.github.com>
2023-03-15 14:54:37 +0800

    Remove git from PATH

--- scripts/hats_shell_ps.ps1
+++ scripts/hats_shell_ps.ps1
@@ -21,9 +21,6 @@ $env:Path = "$path_to_hats\node_modules\.bin;./node_modules/.bin;$env:Path";
 echo "INFO: Set path to npm-global for this session"
 $env:Path = "$path_to_hats\npm-global;$path_to_hats\npm-global\bin;$env:Path";
 
-echo "INFO: Set path to git for this session"
-$env:Path = "$path_to_hats\git\bin;$env:Path";
-
 echo "INFO: Set path to Playwright cache for this session"
 $env:PLAYWRIGHT_BROWSERS_PATH = "$path_to_hats\ms-playwright"
 
