23f9760f2d81cd00e47db5c1021a2dd15655a644
Georgetxm <xuanmingt@gmail.com>
2023-07-14 16:48:26 +0800

    prod formn link update

--- constants/constants.js
+++ constants/constants.js
@@ -238,7 +238,7 @@ export const impactOrder = {
 };
 
 export const formDataFields = {
-  formUrl: `https://docs.google.com/forms/d/e/1FAIpQLSem5C8fyNs5TiU5Vv2Y63-SH7CHN86f-LEPxeN_1u_ldUbgUA/viewform`,
+  formUrl: `https://docs.google.com/forms/d/e/1FAIpQLSem5C8fyNs5TiU5Vv2Y63-SH7CHN86f-LEPxeN_1u_ldUbgUA/formResponse`,
   websiteUrlField: 'entry.1562345227',
   scanTypeField: 'entry.1148680657',
   emailField: 'entry.52161304',
