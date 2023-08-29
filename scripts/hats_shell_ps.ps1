de3ca1d9804fef67537e4c45e17a360e1442ca54
younglim <younglim@users.noreply.github.com>
2023-08-29 15:45:44 +0800

    Set path to JDK and VeraPDF on Windows

--- scripts/hats_shell_ps.ps1
+++ scripts/hats_shell_ps.ps1
@@ -22,6 +22,13 @@ echo "INFO: Set path to Playwright cache for this session"
 $env:PLAYWRIGHT_BROWSERS_PATH = "$path_to_hats\ms-playwright"
 $env:PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD = "true"
 
+echo "INFO: Set path to JDK for this session"
+$env:JAVA_HOME = "$path_to_hats\jdk"
+$env:Path = "$env:JAVA_HOME\bin;$env:Path"
+
+echo "INFO: Set path to VeraPDF for this session"
+$env:Path = "$path_to_hats\verapdf;$env:Path"
+
 if (Test-Path purple-hats) {
    echo "INFO: Set path to purple-hats for this session"
 	$env:Path = "$path_to_hats\purple-hats;$env:Path"	
