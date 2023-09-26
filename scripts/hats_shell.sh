ca55b6102890b96e0fb152130b22241c15f75a81
younglim <younglim@users.noreply.github.com>
2023-09-26 13:27:41 +0800

    Run x64 node to install deps

--- scripts/hats_shell.sh
+++ scripts/hats_shell.sh
@@ -4,11 +4,17 @@ echo "hats Shell - Created By younglim - NO WARRANTY PROVIDED"
 echo "================================================================"
 echo ""
 
+echo "Running as $(uname -m)"
+
 CURR_FOLDERNAME=$(basename "$PWD")
 if [[ $CURR_FOLDERNAME = "scripts" ]]; then
   cd ..
   CURR_FOLDERNAME=$(basename "$PWD")
 fi
+
+# Get current shell command
+SHELL_COMMAND=$(ps -o comm= -p $$)
+SHELL_NAME="${SHELL_COMMAND#-}"
 
 if [[ $(uname -m) == 'arm64' ]]; then
     export ROSETTA2_STATUS_RESULT=$(/usr/bin/pgrep -q oahd && echo true || echo false)
@@ -16,36 +22,40 @@ if [[ $(uname -m) == 'arm64' ]]; then
         echo "Installing Rosetta 2 dependency"
         /usr/sbin/softwareupdate --install-rosetta --agree-to-license
     fi
-fi
 
-echo "INFO: Setting path to node for this session"
-if [[ $(uname -m) == 'arm64' ]]; then
-    export PATH_TO_NODE="$(pwd)/nodejs-mac-arm64/bin"
-    export PATH="$PATH_TO_NODE:$PATH" 
+    arch -x86_64 $SHELL_NAME "$0" "$@"
+    
 else
-    export PATH_TO_NODE="$(pwd)/nodejs-mac-x64/bin"
-    export PATH="$PATH_TO_NODE:$PATH"
+
+    echo "INFO: Setting path to node for this session"
+    if [[ $(uname -m) == 'arm64' ]]; then
+        export PATH_TO_NODE="$(pwd)/nodejs-mac-arm64/bin"
+        export PATH="$PATH_TO_NODE:$PATH" 
+    else
+        export PATH_TO_NODE="$(pwd)/nodejs-mac-x64/bin"
+        export PATH="$PATH_TO_NODE:$PATH"
+    fi
+
+    echo "INFO: Set path to node_modules for this session"
+    if find ./purple-hats -name "node_modules" -maxdepth 1 -type l -ls &> /dev/null; then
+        export PATH="$PWD/purple-hats/node_modules/.bin:$PATH"
+    else
+        export PATH="$PWD/node_modules/.bin:$PATH"
+    fi
+
+    echo "INFO: Set path to Java JRE"
+    export JAVA_HOME="$(PWD)/jre"
+    export PATH="$JAVA_HOME/bin:$PATH"
+
+    echo "INFO: Set path to VeraPDF"
+    export PATH="$PWD/verapdf:$PATH"
+
+    echo "INFO: Set path to Playwright cache for this session"
+    export PLAYWRIGHT_BROWSERS_PATH="$PWD/ms-playwright"
+    export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD="true"
+
+    echo "INFO: Removing com.apple.quarantine attributes for required binaries to run"
+    xattr -rd com.apple.quarantine . &>/dev/null
+
+    $@
 fi
-
-echo "INFO: Set path to node_modules for this session"
-if find ./purple-hats -name "node_modules" -maxdepth 1 -type l -ls &> /dev/null; then
-    export PATH="$PWD/purple-hats/node_modules/.bin:$PATH"
-else
-    export PATH="$PWD/node_modules/.bin:$PATH"
-fi
-
-echo "INFO: Set path to Java JRE"
-export JAVA_HOME="$(PWD)/jre"
-export PATH="$JAVA_HOME/bin:$PATH"
-
-echo "INFO: Set path to VeraPDF"
-export PATH="$PWD/verapdf:$PATH"
-
-echo "INFO: Set path to Playwright cache for this session"
-export PLAYWRIGHT_BROWSERS_PATH="$PWD/ms-playwright"
-export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD="true"
-
-echo "INFO: Removing com.apple.quarantine attributes for required binaries to run"
-xattr -rd com.apple.quarantine . &>/dev/null
-
-$@
