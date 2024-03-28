e2e0a0247a6ab206f425c722f1533f9d92dc58ba
Joshua Lai <joshualai9922@gmail.com>
2024-03-28 17:55:36 +0800

    deletion of browser data

--- cli.js
+++ cli.js
@@ -356,7 +356,8 @@ const scanInit = async argvs => {
 
   // File clean up after url check
   // files will clone a second time below if url check passes
-  deleteClonedProfiles(argvs.browserToRun);
+  
+  !process.env.PURPLE_A11Y_VERBOSE ? deleteClonedProfiles(argvs.browserToRun) : undefined;
 
   if (argvs.exportDirectory) {
     constants.exportDirectory = argvs.exportDirectory;
@@ -399,7 +400,7 @@ const scanInit = async argvs => {
   }
 
   // Delete cloned directory
-  deleteClonedProfiles(data.browser);
+  process.env.PURPLE_A11Y_VERBOSE ? deleteClonedProfiles(data.browser,data.randomToken): deleteClonedProfiles(data.browser)
 
   // Delete dataset and request queues
   await cleanUp(data.randomToken);
