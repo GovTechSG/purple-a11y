e36c00ea41b9c72671a99d58e0c91fe554adec58
angyonghaseyo <chiaangyong@gmail.com>
2024-04-30 13:30:44 +0800

    improv/final_docscan: Remove unnecessary code

--- constants/common.js
+++ constants/common.js
@@ -218,8 +218,7 @@ export const isFileSitemap = async filePath => {
   const file = fs.readFileSync(filePath, 'utf8');
   const isLocalSitemap = await isSitemapContent(file);
   let isLocalFileOrSitemap = isLocalSitemap ? filePath : null;
-  let isLocalFileOption = filePath.endsWith('.pdf') ? filePath : null;
-  return (isLocalFileOption || isLocalFileOrSitemap) ? filePath : null;
+  return (isLocalFileOrSitemap) ? filePath : null;
 };
 
 export const getUrlMessage = scanner => {
