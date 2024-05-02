097bc1dd24c4d45c461ff74c17956169a54a5624
angyonghaseyo <chiaangyong@gmail.com>
2024-05-02 15:27:11 +0800

    improv/final_docscan: Revert cherio.load fix

--- constants/common.js
+++ constants/common.js
@@ -877,12 +877,8 @@ export const getLinksFromSitemap = async (
     } else {
       data = fs.readFileSync(url, 'utf8');
     }
-    let $;
-    if (typeof data === 'string') {
-      $ = cheerio.load(data, { xml: true });
-    } else {
-      $ = cheerio.load(data.toString(), { xml: true });
-    }
+    const $ = cheerio.load(data.toString(), { xml: true });
+    
     // This case is when the document is not an XML format document
     if ($(':root').length === 0) {
 
