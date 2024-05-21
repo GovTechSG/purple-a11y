9c52dead67cd312b9ab22b4b5fd93dcf028fa040
Sheng Hau <shioju12@gmail.com>
2024-05-21 15:41:08 +0800

    fix: unable to run eslint (#329)
    * fix: eslint was not working
    
    * feat: upgrade to eslint flat config
    
    * fix: use latest ecma

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import globals from 'globals';
+import js from '@eslint/js';
+import pluginImport from 'eslint-plugin-import';
+import pluginPrettier from 'eslint-plugin-prettier';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const compat = new FlatCompat();
+
+export default [
+  js.configs.recommended,
+  ...compat.extends('airbnb-base', 'plugin:prettier/recommended'),
+  {
+    languageOptions: { globals: globals.node, ecmaVersion: 'latest', sourceType: 'module' },
+    plugins: {
+      pluginImport,
+      pluginPrettier,
+    },
+  },
+];
