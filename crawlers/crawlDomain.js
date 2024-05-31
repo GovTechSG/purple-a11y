2ab42cd1b1492247d753be737d293056a5a9b6ac
angyonghaseyo <chiaangyong@gmail.com>
2024-05-31 10:30:08 +0800

    improv/executiondestroyed: MAS bug fix

--- crawlers/crawlDomain.js
+++ crawlers/crawlDomain.js
@@ -228,7 +228,7 @@ const crawlDomain = async (
       page.on('request', async request => {
         try {
           // Intercepting requests to handle cases where request was issued before the frame is created
-          await page.context().route(request.url(), async route => {
+          await page.context().route(encodeURI(request.url()), async route => {
             const isTopFrameNavigationRequest = () => {
               return route.request().isNavigationRequest()
                 && route.request().frame() === page.mainFrame();
@@ -384,7 +384,7 @@ const crawlDomain = async (
             new Promise((resolve) => setTimeout(resolve, timeout))
           ]);
         }
-
+        
         await waitForPageLoaded(page, 10000);
         let actualUrl = request.url;
 
