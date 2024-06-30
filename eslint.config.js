19df6f787b84dabc937c334db2fef52dc4228bf6
Sheng Hau <shioju12@gmail.com>
2024-06-30 18:16:29 +0800

    chore: enable linting of ts files
    - ignore files in ./dist

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,19 @@
+// @ts-check
 import globals from 'globals';
-import js from '@eslint/js';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 import pluginImport from 'eslint-plugin-import';
 import pluginPrettier from 'eslint-plugin-prettier';
 import { FlatCompat } from '@eslint/eslintrc';
 
 const compat = new FlatCompat();
 
-export default [
-  js.configs.recommended,
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ['dist/'],
+  },
   ...compat.extends('airbnb-base', 'plugin:prettier/recommended'),
   {
     languageOptions: { globals: globals.node, ecmaVersion: 'latest', sourceType: 'module' },
@@ -19,4 +25,4 @@ export default [
       'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
     },
   },
-];
+);
