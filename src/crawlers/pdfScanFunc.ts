90853c5e076c506359ebf4d5525985839e6914de
younglim <younglim@users.noreply.github.com>
2024-07-29 17:40:12 +0800

    Fix verapdf to support current version at https://github.com/GovTechSG/purple-a11y/releases/download/cache/verapdf-installer.zip

--- src/crawlers/pdfScanFunc.ts
+++ src/crawlers/pdfScanFunc.ts
@@ -311,7 +311,7 @@ export const runPdfScan = async (randomToken: string) => {
   // const veraPdfProfile = getVeraProfile();
   const veraPdfProfile =
     '"' +
-    path.join(execFile, '..', 'profiles/veraPDF-validation-profiles-rel-1.24/PDF_UA/WCAG-21.xml') +
+    path.join(execFile, '..', 'profiles/veraPDF-validation-profiles-rel-1.26/PDF_UA/WCAG-2-2.xml') +
     '"';
   if (!veraPdfExe || !veraPdfProfile) {
     process.exit(1);
