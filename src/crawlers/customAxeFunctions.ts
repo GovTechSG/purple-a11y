a75087d585bcd14426dbcc8681508b8caedac17c
KCas <78678795+CKodes@users.noreply.github.com>
2024-09-10 11:18:47 +0800

    feat: extracted custom functions for axe.config

---
+++ src/crawlers/customAxeFunctions.ts
@@ -0,0 +1,33 @@
+// Custom Axe Functions for axe.config
+export const customAxeConfig = {
+  branding: {
+    application: 'purple-a11y',
+  },
+  checks: [
+    {
+      id: 'oobee-confusing-alt-text',
+      metadata: {
+        impact: 'serious',
+        messages: {
+          pass: 'The image alt text is probably useful.',
+          fail: "The image alt text set as 'img', 'image', 'picture', 'photo', or 'graphic' is confusing or not useful.",
+        },
+      },
+    },
+  ],
+  rules: [
+    { id: 'target-size', enabled: true },
+    {
+      id: 'oobee-confusing-alt-text',
+      selector: 'img[alt]',
+      enabled: true,
+      any: ['oobee-confusing-alt-text'],
+      tags: ['wcag2a', 'wcag111'],
+      metadata: {
+        description: 'Ensures image alt text is clear and useful.',
+        help: 'Image alt text must not be vague or unhelpful. customAxeConfig', // Added customAxeConfig string for testing
+        helpUrl: 'https://www.google.com', // Changed website for testing
+      },
+    },
+  ],
+};
