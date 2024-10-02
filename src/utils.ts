c42ec042f58c0934e2971fcf180f4ec61ecae0ab
Wilson WeeSheng Khoo <wilson.khooweesheng@cognizant.com>
2024-10-02 09:14:13 +0800

    Remove unnecessary try/catch wrapper around spawnSync and execSync

--- src/utils.ts
+++ src/utils.ts
@@ -259,14 +259,10 @@ export const zipResults = (zipName, resultsPath) => {
   }
 
   if (os.platform() === 'win32') {
-    try {
-      execSync(
-        `Get-ChildItem -Path "${resultsPath}\\*.*" -Recurse | Compress-Archive -DestinationPath "${zipName}"`,
-        { shell: 'powershell.exe' },
-      );
-    } catch (err) {
-      throw err;
-    }
+    execSync(
+      `Get-ChildItem -Path "${resultsPath}\\*.*" -Recurse | Compress-Archive -DestinationPath "${zipName}"`,
+      { shell: 'powershell.exe' },
+    );
   } else {
     // Get zip command in Mac and Linux
     const command = '/usr/bin/zip';
@@ -281,12 +277,7 @@ export const zipResults = (zipName, resultsPath) => {
       cwd: resultsPath,
     };
 
-    try {
-      // Zip results
-      spawnSync(command, args, options);
-    } catch (err) {
-      throw err;
-    }
+    spawnSync(command, args, options);
   }
 };
 
