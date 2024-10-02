56d19b7c0b1717aab6eac97499435b106b3094b2
Wilson WeeSheng Khoo <wilson.khooweesheng@cognizant.com>
2024-10-02 14:30:45 +0800

    Comment out a chunk of code that is not used
    Waiting confirmation it can be removed

--- src/crawlers/crawlDomain.ts
+++ src/crawlers/crawlDomain.ts
@@ -144,52 +144,51 @@ const crawlDomain = async (
     });
   }
 
-  const httpHeadCache = new Map<string, boolean>();
-  const isProcessibleUrl = async (url: string): Promise<boolean> => {
-    if (httpHeadCache.has(url)) {
-      silentLogger.info('cache hit', url, httpHeadCache.get(url));
-      return false; // return false to avoid processing the url again
-    }
+  // const httpHeadCache = new Map<string, boolean>();
+  // const isProcessibleUrl = async (url: string): Promise<boolean> => {
+  //   if (httpHeadCache.has(url)) {
+  //     silentLogger.info('cache hit', url, httpHeadCache.get(url));
+  //     return false; // return false to avoid processing the url again
+  //   }
 
-    try {
-      const response = await axios.head(url, { headers: { Authorization: authHeader } });
-      const contentType = response.headers['content-type'] || '';
+  //   try {
+  //     const response = await axios.head(url, { headers: { Authorization: authHeader } });
+  //     const contentType = response.headers['content-type'] || '';
 
-      if (!contentType.includes('text/html') && !contentType.includes('application/pdf')) {
-        silentLogger.info(`Skipping MIME type ${contentType} at URL ${url}`);
-        httpHeadCache.set(url, false);
-        return false;
-      }
+  //     if (!contentType.includes('text/html') && !contentType.includes('application/pdf')) {
+  //       silentLogger.info(`Skipping MIME type ${contentType} at URL ${url}`);
+  //       httpHeadCache.set(url, false);
+  //       return false;
+  //     }
 
-      // further check for zip files where the url ends with .zip
-      if (url.endsWith('.zip')) {
-        silentLogger.info(`Checking for zip file magic number at URL ${url}`);
-        // download first 4 bytes of file to check the magic number
-        const response = await axios.get(url, {
-          headers: { Range: 'bytes=0-3', Authorization: authHeader },
-        });
-        // check using startsWith because some server does not handle Range header and returns the whole file
-        if (response.data.startsWith('PK\x03\x04')) {
-          // PK\x03\x04 is the magic number for zip files
-          silentLogger.info(`Skipping zip file at URL ${url}`);
-          httpHeadCache.set(url, false);
-          return false;
-        } else {
-          // print out the hex value of the first 4 bytes
-          silentLogger.info(
-            `Not skipping ${url} as it has magic number: ${response.data.slice(0, 4).toString('hex')}`,
-          );
-        }
-      }
-    } catch (e) {
-      silentLogger.error(`Error checking the MIME type of ${url}: ${e.message}`);
-      // when failing to check the MIME type (e.g. need to go through proxy), let crawlee handle the request
-      httpHeadCache.set(url, true);
-      return true;
-    }
-    httpHeadCache.set(url, true);
-    return true;
-  };
+  //     // further check for zip files where the url ends with .zip
+  //     if (url.endsWith('.zip')) {
+  //       silentLogger.info(`Checking for zip file magic number at URL ${url}`);
+  //       // download first 4 bytes of file to check the magic number
+  //       const response = await axios.get(url, {
+  //         headers: { Range: 'bytes=0-3', Authorization: authHeader },
+  //       });
+  //       // check using startsWith because some server does not handle Range header and returns the whole file
+  //       if (response.data.startsWith('PK\x03\x04')) {
+  //         // PK\x03\x04 is the magic number for zip files
+  //         silentLogger.info(`Skipping zip file at URL ${url}`);
+  //         httpHeadCache.set(url, false);
+  //         return false;
+  //       }
+  //       // print out the hex value of the first 4 bytes
+  //       silentLogger.info(
+  //         `Not skipping ${url} as it has magic number: ${response.data.slice(0, 4).toString('hex')}`,
+  //       );
+  //     }
+  //   } catch (e) {
+  //     silentLogger.error(`Error checking the MIME type of ${url}: ${e.message}`);
+  //     // when failing to check the MIME type (e.g. need to go through proxy), let crawlee handle the request
+  //     httpHeadCache.set(url, true);
+  //     return true;
+  //   }
+  //   httpHeadCache.set(url, true);
+  //   return true;
+  // };
 
   const enqueueProcess = async (
     page: Page,
