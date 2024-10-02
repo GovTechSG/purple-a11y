173304d834492e223bfae6d08537529f914d1920
Wilson WeeSheng Khoo <wilson.khooweesheng@cognizant.com>
2024-10-02 09:16:08 +0800

    Replace unary increment operator with += 1
    Improved code readability by using a more explicit increment operation

--- src/utils.ts
+++ src/utils.ts
@@ -324,7 +324,7 @@ export const isFollowStrategy = (link1, link2, rule) => {
 export const retryFunction = async (func, maxAttempt) => {
   let attemptCount = 0;
   while (attemptCount < maxAttempt) {
-    attemptCount++;
+    attemptCount += 1;
     try {
       const result = await func();
       return result;
